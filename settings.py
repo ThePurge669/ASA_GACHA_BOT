ea8fdeb381f9583aebe88cce732c26fbc20f13f1
ThePurge669 <thepurge669@gmail.com>
2025-03-11 14:29:56 -0400

    a

---
+++ settings.py
@@ -0,0 +1,40 @@
+screen_resolution = 1080 # 1440 or 1080 ONLY
+iguanadon = "GACHAIGUANADON"
+drop_off = "GACHADEDI"
+bed_spawn = "GACHARENDER"
+berry_station = "GACHABERRYSTATION"
+grindables = "GACHAGRINDABLES"
+berry_type = "mejoberry"
+
+station_yaw = -36.19    #FOR  gacha tower
+render_pushout = 56.31  #FOR  gacha tower
+
+# station_yaw = 171.73 #FOR purge single player test
+# render_pushout = -10.62 #FOR purge single player test
+
+# station_yaw = 53.06  #FOR OVIS
+# render_pushout = 0  #FOR OVIS
+
+external_berry = False # True or False
+deposit_in_vault = False # cambiar a true si quieres q deposite en las vaults. If false grindeara all y depositara en las 3 nuevas dedis
+height_ele = 3
+height_grind = 3
+command_prefix = "%"
+singleplayer = False
+server_number = 0
+
+# you need to put in you channel IDs and you bot API
+
+log_channel_gacha = 0
+log_active_queue = 0
+log_wait_queue =  0
+discord_api_key = ""
+# The ones ABOVE are for 9307 gacha tower
+
+# log_channel_gacha =   #FOR purge single player test
+# log_active_queue=      #FOR purge single player test
+# log_wait_queue =      #FOR purge single player test
+# discord_api_key = ""    #FOR purge single player test
+
+if __name__ =="__main__":
+    pass
